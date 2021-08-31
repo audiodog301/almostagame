b1e2e2a2fe5bbd1c774d7ee9eb89640063160fa1
BuildTools <unconfigured@null.spigotmc.org>
2021-08-30 23:11:03 -0500

    put all the cpal stuff in annother function

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use std::thread;
-
 mod audiostuff;
-use audiostuff::Saw;
+use audiostuff::{cpal_stuff, Saw};
 
 use macroquad::prelude::*;
 
 #[macroquad::main("almostagame")]
 async fn main() {
-    let mut children = Vec::new();
-    children.push(thread::spawn( move ||  {
-    	#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd"),feature = "jack"))]
-    	// Manually check for flags. Can be passed through cargo with -- e.g.
-    	// cargo run --release --example beep --features jack -- --jack
-    	let host = if std::env::args()
-        	.collect::<String>()
-        	.contains(&String::from("--jack"))
-    	{
-        	cpal::host_from_id(cpal::available_hosts()
-            	.into_iter()
-            	.find(|id| *id == cpal::HostId::Jack)
-            	.expect(
-                	"make sure --features jack is specified. only works on OSes where jack is available",
-            	)).expect("jack host unavailable")
-    	} else {
-        	cpal::default_host()
-    	};
-
-    	#[cfg(any(not(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd")),not(feature = "jack")))]
-    	let host = cpal::default_host();
-
-    	let device = host
-        	.default_output_device()
-        	.expect("failed to find a default output device");
-    	let config = device.default_output_config().unwrap();
-
-    	match config.sample_format() {
-        	cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
-        	cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
-        	cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
-    	};}));
+    cpal_stuff();
 
     loop {
         clear_background(RED);
@@ -51,38 +17,4 @@ async fn main() {
 
         next_frame().await
     }
-}
-
-fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig)
-where
-    T: cpal::Sample,
-{
-    let sample_rate = config.sample_rate.0 as f32;
-    let channels = config.channels as usize;
-
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-
-    let mut saw: Saw = Saw {
-        frequency: 110.0,
-        count: 0,
-        val: 0.0,
-    };
-
-    let stream = device
-        .build_output_stream(
-            config,
-            move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-                for frame in data.chunks_mut(channels) {
-                    let value: T = cpal::Sample::from::<f32>(&(saw.next_sample(44_100.0)));
-                    for sample in frame.iter_mut() {
-                        *sample = value;
-                    }
-                }
-            },
-            err_fn,
-        )
-        .unwrap();
-    stream.play();
-
-    loop {}
 }
