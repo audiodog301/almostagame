b07b26de3d1e744e2a8a615f1a17cba5d98c3310
BuildTools <unconfigured@null.spigotmc.org>
2021-08-31 18:50:26 -0500

    neatened graphics

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,9 @@ async fn main() {
     cpal_stuff(receiver.clone());
 
     loop {
-        clear_background(RED);
+        clear_background(BLACK);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+        draw_text("click to make a sound", 10.0, 20.0, 20.0, WHITE);
 
         if is_mouse_button_down(MouseButton::Left) {
             sender.send(Instruction::Set(1));
